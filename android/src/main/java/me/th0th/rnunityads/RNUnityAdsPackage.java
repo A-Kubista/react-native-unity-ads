7f7d56404bc510c70ff9b8755d50760909c37a41
Aleksander Kubista <aleksander.kubista@student.put.poznan.pl>
2018-07-12 17:42:07 +0200

    Update RNUnityAdsPackage.java

--- android/src/main/java/me/th0th/rnunityads/RNUnityAdsPackage.java
+++ android/src/main/java/me/th0th/rnunityads/RNUnityAdsPackage.java
@@ -6,23 +6,24 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class RNUnityAdsPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new RNUnityAdsModule(reactContext));
-    }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Arrays.<ViewManager>asList();
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RNUnityAdsModule(reactContext));
+
+        return modules;
     }
 }
